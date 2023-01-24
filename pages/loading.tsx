c830b5d83ef01a89b065560a1d10f955092f749f
RKPinata <teukutiga@gmail.com>
2023-01-24 23:42:19 +0800

    added loading

--- pages/loading.tsx
+++ pages/loading.tsx
@@ -1,27 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react'
 
-const Home: NextPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/nft/user');
-  },[]);
-
-
+function loading() {
   return (
-    <div className="">
-      <Head>
-        <title>DeGods RipOff</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* <h1 className='text4xl text-red-500'>NFT Drop Site</h1> */}
-      
-      <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen'>
         <div role="status">
             <svg aria-hidden="true" class="w-32 h-32 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -30,10 +11,7 @@ const Home: NextPage = () => {
             <span class="sr-only">Loading...</span>
         </div>
       </div>
-
-
-    </div>
   )
 }
 
-export default Home
+export default loading

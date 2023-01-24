e5259a702bef4597b3fd74254e43e14e1881f17e
RKPinata <teukutiga@gmail.com>
2023-01-24 22:55:20 +0800

    auth done

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react"
 
 function NFTDropPage() {
+
+  // Auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+
+  //showAddress
+  console.log(address);
+
   return (
     <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
       {/* Left */}
@@ -29,13 +39,22 @@ function NFTDropPage() {
       <div className='lg:col-span-6  flex flex-1 flex-col p-12'>
         {/* Header */}
         <header className='flex items-center justify-between'>
-          <h1 className='w-52 sm:w-80 cursor-pointer text-xl font-extralight'>The{' '}
+          <h1 className='w-52 lg:w-96 cursor-pointer text-xl font-extralight'>The{' '}
           <span className='font-extrabold underline decoration-pink-600/50'>DegodsRipoff</span>{' '}
           NFT MarketPlace</h1>
-          <button className='rounded-full bg-red-600 px-4 py-2 text-xs font-bold text-white lg-px-5 lg:py-3'>Sign In</button>
+          <button 
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className='rounded-full bg-red-600 px-4 py-2 text-xs font-bold text-white lg-px-5 lg:py-3'>
+            {address ? 'Sign Out' : 'Sign In'}    
+          </button>
         </header>
 
         <hr className='my-2 border'/>
+        {address && (
+          <p className='text-center '>You're logged in with wallet {address.substring(0,5)}...{address.substring(address.length - 5)} 
+          </p>
+        )}
+
         {/* Content */}
         <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center' >
           <img 
